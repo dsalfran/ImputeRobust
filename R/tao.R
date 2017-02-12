d84ac06cdb5850faa5a6fae62674572a0f45219d
Daniel Salfran <danielsalfran@gmail.com>
2017-02-12 21:45:09 +0100

    New example with data from the TAO project

---
+++ R/tao.R
@@ -0,0 +1,29 @@
+#' Tropical Atmosphere Ocean (TAO) project data
+#'
+#' A sample from the Tropical Atmosphere Ocean (TAO) project data,
+#' downloaded from the \code{GGOBI} project.
+#'
+#' All cases recorded for five locations and two time periods.
+#'
+#' @name tao
+#' @docType data
+#' @format A data frame with 736 observations on the following 8 variables.
+#' \describe{ \item{Year}{a numeric vector} \item{Latitude}{a
+#' numeric vector} \item{Longitude}{a numeric vector}
+#' \item{Sea.Surface.Temp}{a numeric vector} \item{Air.Temp}{a
+#' numeric vector} \item{Humidity}{a numeric vector}
+#' \item{UWind}{a numeric vector} \item{VWind}{a numeric
+#' vector} }
+#' @source \url{https://github.com/ggobi/ggobi/blob/master/data/tao.csv}
+#' @keywords datasets
+#' @examples
+#'
+#' \dontrun{
+#' require(lattice)
+#'
+#' imputed.sets <- mice(tao, method = "gamlss")
+#'
+#' stripplot(imputed.sets)
+#' }
+#'
+NULL
